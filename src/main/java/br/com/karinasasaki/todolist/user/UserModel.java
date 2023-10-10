01ef20a12fca47721f030040722942d1acc677d8
Karina Sasaki <karinasasaki99@gmail.com>
2023-10-10 18:05:22 -0400

    Getters e Setters

--- src/main/java/br/com/karinasasaki/todolist/user/UserModel.java
+++ src/main/java/br/com/karinasasaki/todolist/user/UserModel.java
@@ -1,8 +1,11 @@
 package br.com.karinasasaki.todolist.user;
 
+import lombok.Data;
+
+@Data
 
 public class UserModel {
-  public String username;
-  public String name;
-  public String password;
+  private String username;
+  private String name;
+  private String password;
 }
